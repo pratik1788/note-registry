87e3f80cdd9903a8166cbe99dcb123e2b646e9a9
pratiksurti <pratik.surti17@gmail.com>
2021-08-08 11:36:08 -0400

    Initial Commit

---
+++ src/main/java/com/pratik/noteregistry/NoteRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.pratik.noteregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteRegistryApplication.class, args);
+	}
+
+}
